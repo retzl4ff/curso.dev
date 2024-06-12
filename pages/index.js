717ebeb5aa1f134e1cb6fb5af65fd2c511d8efa7
João Alexandre Retzlaff de Matos <joaoaretzlaff@gmail.com>
2024-06-12 23:11:06 +0000

    modifica a mensagem com uma pergunta haha

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Fala Erick, espero que esteja vendo essa página sem problemas!</h1>
+    return <h1>Fala Erick, como tu tá bodinho? Espero que esteja vendo essa página sem problemas!!!</h1>
 }
 
 export default Home;
