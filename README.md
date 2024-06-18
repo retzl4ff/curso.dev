0750174c2b40c6f6398dbba7c2b966e97968060e
João Alexandre Retzlaff de Matos <joaoaretzlaff@gmail.com>
2024-06-18 01:39:18 +0000

    adiciona `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # curso.dev
+
 Esse repositório servirá como uma base para acompanhar meus estudos e vê-lo progredindo a medida que commits serão feitos e novas funcionalidades serão adicionadas!
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
-function Home(){
-    return <h1>Fala Erick, como tu tá bodinho? Espero que esteja vendo essa página sem problemas haha!<br/><br/><br/>Rebenta o pai?</h1>
+function Home() {
+  return (
+    <h1>
+      Fala Erick, como tu tá bodinho? Espero que esteja vendo essa página sem
+      problemas haha!
+      <br />
+      <br />
+      <br />
+      Rebenta o pai?
+    </h1>
+  );
 }
 
 export default Home;
